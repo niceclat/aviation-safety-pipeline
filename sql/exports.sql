84bd40d92b920bbaf44d6a68b2e3073ba9c62052
veldtlabs <veldtlabs@gmail.com>
2026-04-13 22:25:53 -0400

    Fix export queries for 3-schema bronze, full end-to-end Docker test passing

--- sql/exports.sql
+++ sql/exports.sql
@@ -1,11 +1,14 @@
 -- Export queries for CSV output.
 -- Each query is separated by a comment with the target filename.
 
--- FILE: bronze_events_sample.csv
-SELECT * FROM bronze.events LIMIT 500;
+-- FILE: bronze_avall_events_sample.csv
+SELECT * FROM bronze_avall.events LIMIT 500;
 
--- FILE: bronze_aircraft_sample.csv
-SELECT * FROM bronze.aircraft LIMIT 500;
+-- FILE: bronze_pre2008_events_sample.csv
+SELECT * FROM bronze_pre2008.events LIMIT 500;
+
+-- FILE: bronze_pre1982_sample.csv
+SELECT * FROM bronze_pre1982.tbl_first_half LIMIT 500;
 
 -- FILE: silver_events_enriched_sample.csv
 SELECT * FROM silver.events_enriched LIMIT 500;
